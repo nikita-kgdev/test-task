e92da9e94675df7b1c8fbba7ad0f5b362689d2fb
nikita-kgdev <113676912+nikita-kgdev@users.noreply.github.com>
2023-08-21 03:55:39 +0300

    Delete src/shared/services directory

--- src/shared/services/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Session } from '@src/shared/interfaces/session';
-import { getItem, setItem } from '@src/shared/storage/localStorage';
-
-const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN_KEY'
-
-export const setAuth = (session:Session) => setItem(ACCESS_TOKEN_KEY, session.accessToken);
-export const getAuth = () => getItem(ACCESS_TOKEN_KEY);
