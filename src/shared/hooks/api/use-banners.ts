639da4c115427d13452aad0ec22d5f61ff056861
nikita-kgdev <113676912+nikita-kgdev@users.noreply.github.com>
2023-08-21 03:55:08 +0300

    Delete src/shared/hooks directory

--- src/shared/hooks/api/use-banners.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getBanners } from '@src/shared/services/api/banner';
-
-export const BANNERS_KEY = 'banners'
-
-export const useBanners = () => {
-  return useQuery([BANNERS_KEY], getBanners);
-}
